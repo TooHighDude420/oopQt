622eea1c82f11c42a4b8a5edac672907327ca540
TooHighDude420 <nataro112@gmail.com>
2026-04-17 11:54:00 +0200

    fixed gitignore, added assets and fixed reame

--- modules/player.py
+++ modules/player.py
@@ -2,11 +2,20 @@ from .hand import Hand
 from .card import Card
 
 class Player():
-    def __init__(self):
-        self.hand:Hand = Hand()
+    def __init__(self, chips:int):
+        self.__hands:list[Hand] = []
+        self.__hands.append(Hand())
+        self.__chips:int = chips
+        self.allowed_play:bool = True
 
     def hit(self, card: Card) -> None:
-        self.hand.hit(card)
+        self.__hands[0].hit(card)
 
     def stand(self) -> None:
-        self.hand.stand()
+        self.__hands[0].stand()
+
+    def place_bets(self, amount:int):
+        return NotImplementedError("place_bet is not implemented")
+
+    def reset(self):
+        return NotImplementedError("reset is not implemented")
